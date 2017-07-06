1abdffc9b26d2b6292b9af562c94e4cbd3a33421
yujiuren <354040278@qq.com>
2017-07-06 10:34:26 +0800

    chenp

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -52,12 +52,17 @@
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
+
                         @else
+                            <li class="dropdown">
+                                <a href="{{ url('/contact') }}">
+                                    {{ dump(Auth::guest()) }}
+                                </a>
+                            </li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
-
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
@@ -77,7 +82,6 @@
                 </div>
             </div>
         </nav>
-
         @yield('content')
     </div>
 
