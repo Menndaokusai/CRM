c326a26aeb099f120592fad22e67817bf2a5eef1
whatwhatwhattttt <475811626@qq.com>
2017-07-13 14:39:55 +0800

    Merge branch 'master' of E:\runspace\CRM with conflicts.

--- resources/views/admin/layout/main.blade.php
+++ resources/views/admin/layout/main.blade.php
@@ -1,15 +1,17 @@
-
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <titleTCS管理后台</title>
+
+    <title>TCS管理后台</title>
     <!-- Tell the browser to be responsive to screen width -->
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
     <!-- Bootstrap 3.3.6 -->
+
     <link rel="stylesheet" href="/adminlte/bootstrap/css/bootstrap.min.css">
+    <link href="/css/font-awesome.css" rel="stylesheet">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css">
     <!-- Ionicons -->
@@ -31,6 +33,14 @@
     <link rel="stylesheet" href="/adminlte/plugins/daterangepicker/daterangepicker.css">
     <!-- bootstrap wysihtml5 - text editor -->
     <link rel="stylesheet" href="/adminlte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css">
+    <!-- jqgrid-->
+    <link href="/css/jqgrid/ui.jqgrid.css" rel="stylesheet" type="text/css">
+    <!--tcs框架-->`
+    <link href="/css/animate.css" rel="stylesheet" type="text/css">
+    <link href="/css/style.css" rel="stylesheet" type="text/css">
+
+    <!--自定义样式-->
+    <link href="/css/contacts/contacts.css" rel="stylesheet" type="text/css">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -38,36 +48,54 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+
+    <!-- jQuery 2.2.3 -->
+    <script src="/adminlte/plugins/jQuery/jquery-2.2.3.min.js"></script>
+
+    {{--jqgrid--}}
+    <script src="/js/plugins/jqgrid/i18n/grid.locale-cn.js?0820"></script>
+    <script src="/js/jqgrid/jquery.jqGrid.min.js"></script>
+
+    <script src="/js/contact/jqgridListShow.js"></script>
+
+
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
 
-    @include("admin.layout.header")
-    <!-- Left side column. contains the logo and sidebar -->
-    @include("admin.layout.sidebar")
-    <!-- Content Wrapper. Contains page content -->
+@include("admin.layout.header")
+<!-- Left side column. contains the logo and sidebar -->
+@include("admin.layout.sidebar")
+<!-- Content Wrapper. Contains page content -->
     <div class="content-wrapper">
 
-        @yield("content")
-        <!-- /.content -->
+    @yield("content")
+    <!-- /.content -->
     </div>
     <!-- /.content-wrapper -->
-    @include("admin.layout.footer")
-    <!-- /.control-sidebar -->
+@include("admin.layout.footer")
+<!-- /.control-sidebar -->
     <!-- Add the sidebar's background. This div must be placed
          immediately after the control sidebar -->
     <div class="control-sidebar-bg"></div>
 </div>
+
 <!-- ./wrapper -->
 
-<!-- jQuery 2.2.3 -->
-<script src="/adminlte/plugins/jQuery/jquery-2.2.3.min.js"></script>
+{{--tcs框架--}}
+<script src="/js/content.js"></script>
+<script src="/js/demo/form-validate-demo.js"></script>
+
+
+<!-- jQuery Validation plugin javascript-->
+<script src="/js/plugins/validate/jquery.validate.min.js"></script>
+<script src="/js/plugins/validate/messages_zh.min.js"></script>
+
 <!-- jQuery UI 1.11.4 -->
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-<script>
-    $.widget.bridge('uibutton', $.ui.button);
-</script>
+<script>$.widget.bridge('uibutton', $.ui.button);</script>
 <!-- Bootstrap 3.3.6 -->
 <script src="/adminlte/bootstrap/js/bootstrap.min.js"></script>
 <!-- Morris.js charts -->
@@ -80,6 +108,7 @@
 <script src="/adminlte/plugins/jvectormap/jquery-jvectormap-world-mill-en.js"></script>
 <!-- jQuery Knob Chart -->
 <script src="/adminlte/plugins/knob/jquery.knob.js"></script>
+
 <!-- daterangepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.11.2/moment.min.js"></script>
 <script src="/adminlte/plugins/daterangepicker/daterangepicker.js"></script>
@@ -98,5 +127,7 @@
 <!-- AdminLTE for demo purposes -->
 <script src="/adminlte/dist/js/demo.js"></script>
 <script src="{{ mix('/js/admin.js') }}"></script>
+
+
 </body>
 </html>
