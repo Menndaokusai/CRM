d5acc05fadff494f137697b6880b24db7c1b803a
Menndaokusai <747284924@qq.com>
2017-07-14 10:16:37 +0800

    ...

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
             return redirect('/admin/home');
         }
 
-        return 123;
+        return \Redirect::back()->withErrors("用户名密码错误");
     }
 
     /*
