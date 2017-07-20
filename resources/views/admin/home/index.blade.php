28a20415d1d6f3b2ff507c858a669f44876217a5
Menndaokusai <747284924@qq.com>
2017-07-20 15:08:30 +0800

    ...

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -7,8 +7,8 @@
 
         <div class="row">
             <div class="col-lg-12">
-                <div class="home_image1" style="background: url('/image/beij.jpg');height: 400px;margin-top: 300px;">
-                    <p class="home_font col-md--6" style=" font-size: 7rem;margin-left: 650px;margin-top: 150px;float: left;width: 700px;height: 300px;">欢迎使用TCS管理平台</p>
+                <div class="home_image1" style="background: url('/image/beij.jpg');height: 400px;margin-top: 150px;">
+                    <p class="home_font col-md--6" style=" font-size: 7rem;margin-left: 350px;margin-top: 150px;float: left;width: 700px;height: 300px;">欢迎使用TCS管理平台</p>
                 </div>
             </div>
         </div>
