ef050a939d6e05da59cc0febabbd58a2bc8c9c3d
whatwhatwhattttt <475811626@qq.com>
2017-07-20 14:11:07 +0800

    主页修改2

--- resources/views/admin/home/index.blade.php
+++ resources/views/admin/home/index.blade.php
@@ -6,9 +6,9 @@
         <!-- Small boxes (Stat box) -->
 
         <div class="row">
-            <div class="cl-lg-12">
-                <div class="home_image1" style="background: url('/image/beij.jpg');height: 400px;margin-top: 15%;">
-                    <p class="home_font" style=" font-size: 7rem;padding-left: 30%;padding-top: 5%;">欢迎使用TCS管理平台</p>
+            <div class="col-lg-12">
+                <div class="home_image1" style="background: url('/image/beij.jpg');height: 400px;margin-top: 300px;">
+                    <p class="home_font col-md--6" style=" font-size: 7rem;margin-left: 650px;margin-top: 150px;float: left;width: 700px;height: 300px;">欢迎使用TCS管理平台</p>
                 </div>
             </div>
 
